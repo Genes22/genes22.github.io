dee2393dd95bc7c39e2de96bbfb9927f5dfd19a3
genes22 <genes0022@gmail.com>
2019-08-12 18:57:00 +0300

    config

--- classmates/wasa/application/config/config.php
+++ classmates/wasa/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/wasa/';
+$config['base_url'] = 'http://localhost:8000/genes22.github.com/classmates/wasa/';
 
 /*
 |--------------------------------------------------------------------------
